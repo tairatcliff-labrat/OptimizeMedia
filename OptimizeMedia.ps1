13d138f50feb32e320ba9ac6d3495557797e7c3b
Tai Ratcliff <32991623+tairatcliff-labrat@users.noreply.github.com>
2018-07-29 23:12:09 +1000

    Update OptimizeMedia.ps1

--- OptimizeMedia.ps1
+++ OptimizeMedia.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 Instructions
 
 Download ffmpeg from 
@@ -16,15 +16,18 @@ Install ffmpeg and add the /bin folder to the Windows path
 
 
 param(
-    [ValidateScript({Test-Path $_})][string]$Directory = "C:\Temp", # The directory to recursively scan for media.
-    [ValidateRange(0,999)][int]$OptimizeAfterDays = 90, # How old a file must be before it is optimized.
+    [ValidateScript({Test-Path $_})][string]$Directory = "C:\Media\Movies", # The directory to recursively scan for media.
+    [ValidateRange(0,999)][int]$OptimizeAfterDays = 0, # How old a file must be before it is optimized.
     [string]$OutputToCsv, # Output the "Processed Videos" report to a csv file. Specify the CSV file to save to i.e. "C:\Temp\Videos.csv". If left blank a CSV won't be generated.  
     [string]$LogFile, #If a logfile is required, enter the directory and logfile name. i.e. "C:\Temp\Videos.log"
-    [ValidateSet("hd480","hd720","hd1080")][string]$ffmpegQuality = "hd480", 
+    [ValidateSet("hd480","hd720","hd1080")][string]$ffmpegQuality = "hd720", 
     [ValidateRange(0,51)][string]$ConstantRateFactor = "21", #A setting between 21 and 24 is good. 0 is lossless and highest quality and 51 is the worst quality. 
     [switch]$ValidateOnly, # Will simulate the script running but will not actually optimize or delete any files. The "Processed Videos" output will still be generated.
     [switch]$DeleteOriginalVideo # Without this parameter in the command the original file will be optimized and the not deleted.
     )
+
+cls
+Write-Host `n`n`n`n`n`n
 
 ## Declare Variables
 $VideoExtensions = "*.mkv","*.mp4","*.avi" # What video file extensions are supported by ffmpeg and you want to be included in the optimisation process. 
@@ -36,6 +39,7 @@ $NumFilesCounter = 0
 $CurrentlyProcessedSize = 0
 $OriginalTotalSize = 0
 $OptimizedTotalFileSize = 0
+$Percent = 0
 $VideosErrored = @()
 $VideosProcessed = @()
 $Files = @()
@@ -54,23 +58,21 @@ function Log-Message([string]$Message) {
 }
 
 ForEach($File in $Files){
-    Write-Host "###################################### Processing New Video ######################################"
+    Write-Host "`n###################################### Processing New Video ######################################"
     $NumFilesCounter ++
     $InputFileName = $File.FullName
     $InputFileBaseName = $File.BaseName
     $InputFileDirectory = $File.DirectoryName
     $OutputFileName = [io.path]::ChangeExtension($File.FullName, "optimized.$ffmpegQuality.mp4")
+    $OutputFileShortName = $OutputFileName.Split("\")[-1] #$OutputFileName.Substring($OutputFileName.LastIndexOf("\"),100)
     $FileAge = $Date - $File.CreationTime
-    Write-Host "$($File.BaseName) is $($FileAge.days) days old"-ForegroundColor Yellow
-    Write-Host
+    Write-Host "`n$($File.BaseName) is $($FileAge.days) days old"-ForegroundColor Yellow
     If ($FileAge.Days -gt $OptimizeAfterDays){
         Write-Host "Optimzing any videos that are older than $OptimizeAfterDays days, The video is old enough to be optimized" -ForegroundColor Yellow
         $InputFileDuration = &ffprobe.exe $EscapeParser " -v error -show_format -select_streams v `"$InputFileName`"" | Select-String "duration="
         $InputFileDurationTime = New-TimeSpan -Seconds (($InputFileDuration.ToString()).Split('=')[1].Split('.')[0])
         $InputFileSize = $File.Length.ToString()
-        Write-Host
-        Write-Host "Input video duration is (hh:mm:ss):" $InputFileDurationTime.ToString("hh\:mm\:ss") -ForegroundColor Yellow
-        Write-Host
+        Write-Host "`nInput video duration is (hh:mm:ss):" $InputFileDurationTime.ToString("hh\:mm\:ss") -ForegroundColor Yellow
 
         $Props = @{}
         $Props.Add("File Name", $InputFileBaseName)
@@ -82,50 +84,100 @@ ForEach($File in $Files){
 
         $OptimizeVideoTimer = [Diagnostics.stopwatch]::StartNew()
         If(!($ValidateOnly)){
-            $OptimizeVideo = &ffmpeg.exe $EscapeParser " -y -i `"$InputFileName`" -s $ffmpegQuality -c:v libx264 -crf $ConstantRateFactor -b:v 200k -b:a 128k -c:a aac -strict -2 `"$OutputFileName`""
-            #$Params = "-y -i `"$InputFileName`" -s hd480 -c:v libx264 -crf 24 -b:v 200k -b:a 128k -c:a aac -strict -2 `"$OutputFileName`""
-            #Start-Process ffmpeg.exe -ArgumentList $Params -PassThru -Wait
+            $ffprobeBlock = {
+            Param(
+                $InputFileName
+            )
+            ffprobe.exe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate,nb_frames "$InputFileName" 2>&1
+            }
+            
+            Write-Host "`nProcessing $($File.Name)" -ForegroundColor Green
+
+            $InputFileTotalFrames = Invoke-Command -ScriptBlock $ffprobeBlock -ArgumentList $InputFileName 
+            [string]$InputFileNbFrames = $InputFileTotalFrames[1]
+            [string]$InputFileFrameRate = $InputFileTotalFrames[0] 
+
+            if($InputFileNbFrames.ToUpper() -ne "N/A"){
+                [int]$InputFileTotalFramesInt = $InputFileTotalFrames.Trim().Split('=')[1]
+            } else {
+                Write-Host "`nTotal frame count unavailable, estimating the total frames based on the frame rate and duration." -ForegroundColor Red
+                [int]$FrameRate = $InputFileFrameRate.Split("/")[0] / $InputFileFrameRate.Split("/")[1]
+                [int]$InputFileTotalFramesInt = $InputFileDurationTime.TotalSeconds * $FrameRate
+            }
+
+            $ffmpegBlock = {
+            Param(
+                [string]$InputFileName, 
+                [string]$ffmpegQuality, 
+                [int]$ConstantRateFactor, 
+                [string]$OutputFileName
+            )
+            ffmpeg.exe -y -i "$InputFileName" -s $ffmpegQuality -c:v libx264 -crf $ConstantRateFactor -b:v 200k -b:a 128k -c:a aac -strict -2 "$OutputFileName" 4>&1
+            } 
+            
+            $OptimizeVideo = Start-Job -ScriptBlock $ffmpegBlock -ArgumentList $InputFileName, $ffmpegQuality, $ConstantRateFactor, $OutputFileName
+            Write-Progress -Activity "Processed 0% (frame: 0 of $InputFileTotalFramesInt) of Movie: ""$OutputFileShortName""" -PercentComplete 0
+            Start-Sleep 15
+
+            While($OptimizeVideo.State -eq "Running"){
+                [string]$OptimizeVideoOutput = Receive-Job $OptimizeVideo 2>&1
+                [string]$CurrentFrame = $OptimizeVideoOutput.Substring(($OptimizeVideoOutput.LastIndexOf("frame=") + 6), 20)
+                [int]$CurrentFrameInt = $CurrentFrame.Substring(0, $CurrentFrame.IndexOf("f"))
+                $Percent = [math]::Round($CurrentFrameInt / $InputFileTotalFramesInt * 100, 2)
+                If ($Percent -gt 100){
+                    Write-Host "Current Frame is higher than expected: " $CurrentFrameInt of $InputFileTotalFramesInt
+                    $Percent = 100
+                }
+                Write-Progress -Activity "Processed $Percent% (frame: $CurrentFrameInt of $InputFileTotalFramesInt) of Movie: ""$OutputFileShortName""" -PercentComplete $Percent
+                Start-Sleep 30
+            }
+            Write-Progress -Activity "Processed $Percent% (frame: $CurrentFrameInt of $InputFileTotalFramesInt) of Movie: ""$OutputFileShortName""" -Completed
+            If($JobOutput){Remove-Job $OptimizeVideo}
         }
     } Else {
-        Write-Host "Optimzing any videos that are older than $OptimizeAfterDays days, The video is not old enough to be optimized" -ForegroundColor Yellow
+        Write-Host "`nOptimzing any videos that are older than $OptimizeAfterDays days, The video is not old enough to be optimized" -ForegroundColor Yellow
         Continue
     }
 
     If (Test-Path $OutputFileName){
         $OutputFileDuration = &ffprobe.exe $EscapeParser " -v error -show_format -select_streams v `"$OutputFileName`"" | Select-String "duration="
         $OutputFileDurationTime = New-TimeSpan -Seconds ($OutputFileDuration.ToString()).Split('=')[1].Split('.')[0]
-
+        [string]$OutputFileTotalFrames = Invoke-Command -ScriptBlock $ffprobeBlock -ArgumentList $OutputFileName
+        [string]$OutputFileTotalFrames = $OutputFileTotalFrames.Trim().Split('=')[1]
 
         Write-Host "Input File size is (MB):" ([math]::Round(($InputFileSize / 1MB).ToString(),2)) -ForegroundColor Yellow
         Write-Host "Output File size is (MB):" ([math]::Round(($OutputFileSize / 1MB).ToString(),2)) -ForegroundColor Yellow
-        Write-Host
-        Write-Host "Input video duration (minutes):" $InputFileDurationTime.TotalMinutes -ForegroundColor Yellow
+        Write-Host "`nInput video duration (minutes):" $InputFileDurationTime.TotalMinutes -ForegroundColor Yellow
         Write-Host "Output video duration (minutes):" $OutputFileDurationTime.TotalMinutes -ForegroundColor Yellow
-        Write-Host
 
-        If(([math]::Round($OutputFileDurationTime.TotalMinutes.ToString(),1) -eq [math]::Round($InputFileDurationTime.TotalMinutes.ToString(),1)) -and ($OutputFileDuration -ne $null)){
+        $DurationDifference = [Math]::Abs($OutputFileDurationTime.TotalSeconds - $InputFileDurationTime.TotalSeconds)
+        Write-Host "Input file duration (seconds): "$InputFileDurationTime.TotalSeconds
+        Write-Host "Output file duration (seconds): "$OutputFileDurationTime.TotalSeconds
+        Write-Host "Difference: " $DurationDifference
+
+        If(($DurationDifference -lt 30) -or ($OutputFileDurationTime -gt $InputFileDurationTime)){
             $OutputFileSize = (Get-Item $OutputFileName).Length.ToString()
             $OptimizedTotalFileSize +=  $OutputFileSize
             
             $Props.Add("Optimized Size (MB)", ([math]::Round(($OutputFileSize / 1MB).ToString(),2)))
             
-            Write-Host "Successfully optimized the video" -BackgroundColor Green -ForegroundColor Black
+            Write-Host "`nSuccessfully optimized the video" -BackgroundColor Green -ForegroundColor Black
             Write-Host "The optimized file has been saved to:" $OutputFileName -BackgroundColor Green -ForegroundColor Black
-            Write-Host
-            Write-Host "Attempting to remove file:" $InputFileName
+            Write-Host "`nAttempting to remove file:" $InputFileName
             
             $StopWatch = [Diagnostics.stopwatch]::StartNew()
             While ((Test-Path $InputFileName) -and ($StopWatch.Elapsed -lt $TimeOut)){
                 If($DeleteOriginalVideo){
-                    Remove-Item -Path $InputFileName
+                    Remove-Item -Path $InputFileName -Force
                 } Else {
                     Write-Host "`"Delete Original Video`" is set to False. The original video file has not been deleted." -ForegroundColor Yellow
+                    Break
                 }
                 If (Test-Path $InputFileName){Start-Sleep 5}
             }
             If ($StopWatch.Elapsed -gt $TimeOut){Write-Host "Timed out while trying to delete" $InputFileName -BackgroundColor Red -ForegroundColor Black}
         } Else {
-            Write-Host "An inconsistency has been detected in the output file. As a precaution the input file won't be deleted" -BackgroundColor Yellow -ForegroundColor Black
+            Write-Host "`nAn inconsistency has been detected in the output file. As a precaution the input file won't be deleted" -BackgroundColor Yellow -ForegroundColor Black
             Write-Host "Deleting the output file and keeping the original input file" -BackgroundColor Yellow -ForegroundColor Black
             
             $VideoErrored = @{}
@@ -136,11 +188,11 @@ ForEach($File in $Files){
 
             $StopWatch = [Diagnostics.stopwatch]::StartNew()
             While ((Test-Path $OutputFileName) -and ($StopWatch.Elapsed -lt $TimeOut)){
-                Remove-Item -Path $OutputFileName
+                Remove-Item -Path $OutputFileName -Force
                 If (Test-Path $OutputFileName){
                     Start-Sleep 5
                 }Else{
-                    Write-Host "Successfully deleted:" $OutputFileName -BackgroundColor Yellow -ForegroundColor Black
+                    Write-Host "`nSuccessfully deleted:" $OutputFileName -BackgroundColor Yellow -ForegroundColor Black
                 }
             }
             If ($StopWatch.Elapsed -gt $TimeOut){Write-Host "Timed out while trying to delete" $OutputFileName -BackgroundColor Red -ForegroundColor Black}
@@ -149,25 +201,22 @@ ForEach($File in $Files){
 
     $VideosProcessed += New-Object PSObject -Property $Props
 
-    Write-Host "The time to complete this video was (hh:mm:ss):" $OptimizeVideoTimer.Elapsed.ToString("hh\:mm\:ss") -ForegroundColor Yellow
-    Write-Host 
-    Write-Host "The script has been running for a total time of (hh:mm:ss):" $ScriptRunningTime.Elapsed.ToString("hh\:mm\:ss") -ForegroundColor Yellow
-    Write-Host
-    Write-Host "Processed $NumFilesCounter of $NumFiles videos" -ForegroundColor Cyan
+    Write-Host "`nThe time to complete this video was (hh:mm:ss):" $OptimizeVideoTimer.Elapsed.ToString("hh\:mm\:ss") -ForegroundColor Yellow
+    Write-Host "`nThe script has been running for a total time of (hh:mm:ss):" $ScriptRunningTime.Elapsed.ToString("hh\:mm\:ss") -ForegroundColor Yellow
+    Write-Host "`nProcessed $NumFilesCounter of $NumFiles videos" -ForegroundColor Cyan
     Write-Host "Proccessed" ([math]::Round(($CurrentlyProcessedSize / 1GB),2)) "GB of the total" ([math]::Round(($OriginalTotalSize / 1GB),2)) "GB" -ForegroundColor Cyan
     If(!($ValidateOnly)){
-        Write-Host "The $NumFilesCounter processed files have been reduced from" ([math]::Round(($CurrentlyProcessedSize / 1GB),2)) "GB to:" ([math]::Round(($OptimizedTotalFileSize / 1GB),2)) "GB" -ForegroundColor Cyan
+        Write-Host "`nThe $NumFilesCounter processed files have been reduced from" ([math]::Round(($CurrentlyProcessedSize / 1GB),2)) "GB to:" ([math]::Round(($OptimizedTotalFileSize / 1GB),2)) "GB" -ForegroundColor Cyan
     }
-    Write-Host
 
 }
 Write-Host
 If($VideosErrored -ne $null){
-    Write-Host "The following vidoes failed to be processed" -BackgroundColor Red -ForegroundColor Black
+    Write-Host "`nThe following vidoes failed to be processed" -BackgroundColor Red -ForegroundColor Black
     $VideosErrored | FT
 }
 
-Write-Host "The following vidoes have been processed" -BackgroundColor Green -ForegroundColor Black
+Write-Host "`nThe following vidoes have been processed" -BackgroundColor Green -ForegroundColor Black
 $VideosProcessed | Select "File Name", "File Directory", "Age (Days)", "Original Size (MB)", "Optimized Size (MB)" | FT
 If($OutputToCsv){$VideosProcessed | Export-Csv -Path $OutputToCsv -NoTypeInformation}
 
